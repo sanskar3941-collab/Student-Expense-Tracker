e1577acab48626e0e3c7d2221a21e41155d1293a
sanskar3941-collab <sanskar3941@gmail.com>
2026-09-19 17:19:44 -0500

    add menu choice logic

--- expense_tracker.py
+++ expense_tracker.py
@@ -9,4 +9,13 @@ print("4. Exit")
 
 choice = input("Enter your choice: ")
 
-print("You selected:", choice)
+if choice == "1":
+    print("Add Expense selected")
+elif choice == "2":
+    print("View Expenses selected")
+elif choice == "3":
+    print("View Total selected")
+elif choice == "4":
+    print("Goodbye!")
+else:
+    print("Invalid choice")
